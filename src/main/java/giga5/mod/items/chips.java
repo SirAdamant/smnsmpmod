00d38cdb90a2d219fd2761bd2bb8e33efc9d3947
SirAdamant <57964620+SirAdamant@users.noreply.github.com>
2021-03-31 16:26:26 -0700

    Update chips.java

--- src/main/java/giga5/mod/items/chips.java
+++ src/main/java/giga5/mod/items/chips.java
@@ -6,7 +6,7 @@ import net.minecraft.world.World;
 
 public class chips extends Item {
     public chips() {
-        super((new Item.Properties().food(new Food.Builder().hunger(4).saturation(2.2F).build()).containerItem(Items.BOWL).maxStackSize(1)));
+        super((new Item.Properties().food(new Food.Builder().hunger(5).saturation(1.4F).build()).containerItem(Items.BOWL).maxStackSize(1)));
     }    public UseAction UseAction(ItemStack stack) {
         return UseAction.EAT;
     }
